2dc0f0541d37473ebf80413cc5526fbe3d39ebc8
Emiliano <emiliano.hotes@despegar.com>
2022-12-03 11:31:14 -0300

    2021-01 | Contando ovejas para dormir

--- 2021/01/script.js
+++ 2021/01/script.js
@@ -1,4 +1,22 @@
-export default function contarOvejas(ovejas) {
-  // aquí tu magia
-  return ovejas
+function contarOvejas(ovejas) {
+  return ovejas.filter(({color, name}) => {
+    return color === "rojo" && name.toLowerCase().includes("a") && name.toLowerCase().includes("n")
+  })
+
 }
+
+const ovejas = [
+  { name: 'Noa', color: 'azul' },
+  { name: 'Euge', color: 'rojo' },
+  { name: 'Navidad', color: 'rojo' },
+  { name: 'Ki Na Ma', color: 'rojo'},
+  { name: 'AAAAAaaaaa', color: 'rojo' },
+  { name: 'Nnnnnnnn', color: 'rojo'}
+]
+
+const ovejasFiltradas = contarOvejas(ovejas)
+
+console.log(ovejasFiltradas)
+
+// [{ name: 'Navidad', color: 'rojo' },
+//  { name: 'Ki Na Ma', color: 'rojo' }]
